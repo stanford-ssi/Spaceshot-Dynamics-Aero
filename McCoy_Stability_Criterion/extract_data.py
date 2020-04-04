f96c52ec41dbd062a94bfa4565ca7c89133d6565
Ahmed <aiabdalla5050@gmail.com>
2020-04-03 19:54:58 -0700

    i mean it runs...kinda

--- McCoy_Stability_Criterion/extract_data.py
+++ McCoy_Stability_Criterion/extract_data.py
@@ -1,17 +1,18 @@
 import json
+import sys
 
 def flatten(data, prefix=""):
-    flat = []
-    if isinstance(data,dict):
+    flat = {}
+    if isinstance(data, dict):
         if prefix:
             prefix += "."
-        for k,v in data.items():
+        for k, v in data.items():
             flat.update(flatten(v, prefix + str(k)))
-        return flat;
-    if isinstance(data,list):
+        return flat
+    if isinstance(data, list):
         if prefix:
             prefix += "."
-        i = 0;
+        i = 0
         for v in data:
             flat.update(flatten(v, prefix + str(i)))
             i += 1
@@ -41,6 +42,6 @@ def get_column(lines, column):
         except ValueError:
             print('line not JSON')
 
-    data = [[data[key] for data in json_body] for key in ["tick","adxl1.a.0","adxl1.a.1"]]
+    data = [[data[key] for data in json_body] for key in ["tick","adxl1.a.0","adxl1.a.1","adxl1.a.2","bmi1.a.0","bmi1.a.1","bmi1.a.2"]]
     data[0] = [(a)/1000 for a in data[0]]
     return data[0], data[column]
--- McCoy_Stability_Criterion/utility.py
+++ McCoy_Stability_Criterion/utility.py
@@ -1,19 +1,26 @@
-import scipy
+from scipy import integrate
 import numpy as np
 
 def get_data(time, accel):
-    vel = scipy.integrate.cumtrapz(accel, x=time)
-    x_pos = scipy.integrate.cumtrapz(vel, x=time)
+    vel = integrate.cumtrapz(accel, x=time, initial=0)
+    x_pos = np.asarray(integrate.cumtrapz(vel, x=time, initial=0))
 
     #use pressure equations to calculate air density for each x_pos
-    trop_x = np.argmax(x_pos < 11000)
-    rho_trop = 1.225 * (288.15 / (288.15 + -0.0065 * x_pos[:trop_x]) ^ (1 + (9.8 * 0.02896) / (8.3145 * -0.0065))
-    strat_x = np.argmax(x_pos < 20000)
-    rho_strat = 0.364 * np.exp(-(9.8 * 0.02896 * x_pos[trop_x:strat_x]) / (8.3145 * 216.65))
-    # assumed below the mesosphere (32km)
-    rho_mes = 0.088 * (216.65 / (216.65 + 0.001 * x_pos[trop_x:strat_x]) ^ (1 + (9.8 * 0.02896) / (8.3145 * 0.001))
+    rho = []
+    trop_x = np.argmax(x_pos > 11000)
+    if trop_x == 0:
+        trop_x = len(x_pos)
+    rho.extend([1.225 * (288.15 / (288.15 + -0.0065 * x ** (1 + (9.8 * 0.02896) / (8.3145 * -0.0065)))) for x in x_pos[:trop_x]])
 
-    return vel, x_pos, np.append(rho_trop, rho_mes, rho_strat)
+    strat_x = np.argmax(x_pos > 20000)
+    if strat_x == 0:
+        strat_x = len(x_pos)
+    rho.extend([0.364 * np.exp(-(9.8 * 0.02896 * x / (8.3145 * 216.65))) for x in x_pos[trop_x:strat_x]])
+
+    # assumed below the mesosphere (32km)
+    rho.extend([0.088 * (216.65 / (216.65 + 0.001 * x ** (1 + (9.8 * 0.02896) / (8.3145 * 0.001)))) for x in x_pos[trop_x:strat_x]])
+
+    return vel, x_pos, rho
 
 def get_spin(vel, rho):
-    return vel / 0.004 * np.sqrt(2 * rho * -.2352 * 0.0607 * 7 * 0.00607)
+    return [vel[i] / 0.004 * np.sqrt(2 * rho[i] * -.2352 * 0.0607 * 7 * 0.00607) for i in range(len(vel))]
